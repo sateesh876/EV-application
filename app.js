4f49dbc1ffaafe1a0c8c2f6d3a691effce62675b
SATEESH GURIJALA <51813978+sateesh876@users.noreply.github.com>
2021-02-11 12:33:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+import express from 'express';
+import bodyParser from 'body-parser';
+import mysql from 'mysql';
+
+import usersRoutes from './routes/users.js';
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(bodyParser.json())
+
+app.use('/users', usersRoutes);
+//MySQL
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'devices'
+})
+
+app.get('', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        console.log('connected as id ' + connection.threadId)
+        connection.query('SELECT * from cstation_details', (err, rows) => {
+            connection.release() // return the connection to pool
+
+            if (!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+
+            // if(err) throw err
+            console.log('The data from devices table are: \n', rows)
+        })
+    })
+})
 
 
-var products = require('./api/products');
-var orders = require('./api/orders');
-const app = express();
 
-app.use(cors());
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended:false}));
-
-app.use("/products",products);
-app.use("/orders",orders);
-
-//if we are here then the specified request is not found
-app.use((req,res,next)=> {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+app.get('/:name', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        connection.query('SELECT * FROM cstation_details WHERE cstation_name = ?', [req.params.name], (err, rows) => {
+            connection.release() // return the connection to pool
+            if (!err) {
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+            
+            console.log('The data from stations table are: \n', rows)
+        })
+    })
 });
 
-//all other requests are not implemented.
-app.use((err,req, res, next) => {
-   res.status(err.status || 501);
-   res.json({
-       error: {
-           code: err.status || 501,
-           message: err.message
-       }
-   });
+app.post('', (req, res) => {
+
+    pool.getConnection((err, connection) => {
+        if(err) throw err
+        
+        const params = req.body
+        connection.query('INSERT INTO cstation_details SET ?', params, (err, rows) => {
+        connection.release() // return the connection to pool
+        if (!err) {
+            res.send(`Station with the record ID  has been added.`)
+        } else {
+            console.log(err)
+        }
+        
+        console.log('The data from cstation table are:11 \n', rows)
+
+        })
+    })
 });
 
-module.exports = app;
+app.listen(port, () => console.log(`Listening on port ${port}`))
