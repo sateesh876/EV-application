6ba996f70699ecc8d7ca7784421c99355daad61c
SATEESH GURIJALA <51813978+sateesh876@users.noreply.github.com>
2021-02-16 16:08:00 +0530

    Add files via upload

--- user/app/routes/customer.routes.js
+++ user/app/routes/customer.routes.js
@@ -7,14 +7,14 @@ module.exports = app => {
     // Retrieve all Customers
     app.get("/users", users.findAll);
   
-    // Retrieve a single Customer with customerId
-    app.get("/users/:customerId", users.findOne);
+    // Retrieve a single Customer with userId
+    app.get("/users/:userId", users.findOne);
   
-    // Update a Customer with customerId
-    app.put("/users/:customerId", users.update);
+    // Update a Customer with userId
+    app.put("/users/:userId", users.update);
   
-    // Delete a Customer with customerId
-    app.delete("/users/:customerId", users.delete);
+    // Delete a Customer with userId
+    app.delete("/users/:userId", users.delete);
   
     // Create a new Customer
     app.delete("/users", users.deleteAll);
